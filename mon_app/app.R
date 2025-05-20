80b5636a450f5e382cb764a682465c637a7607d4
Simon Penel <simon.penel@univ-lyon1.fr>
2025-05-20 17:13:13 +0200

    Add test functions to  app.R

--- mon_app/app.R
+++ mon_app/app.R
@@ -53,6 +53,27 @@ ui <- bootstrapPage(
   ),
 )
 
+mean_var <- function(x, var) {
+    mean(masting[masting$Arbre == x,][[var]])
+    }
+
+get_summary <- function(m){
+  sum_mast  <- data.frame(m$Arbre,m$Latitude,m$Longitude)
+  sum_mast <- sum_mast[!duplicated(sum_mast), ]
+  print(sum_mast)
+  arbres <- sum_mast$m.Arbre
+  print(arbres)
+
+  test = lapply(arbres,mean_var, var="tauxfructif")
+  sum_mast$meantauxfructif = test
+  test = lapply(arbres,mean_var, var="Total_Flowers_per_m2")
+  sum_mast$meanTotal_Flowers_per_m2 = test
+  test = lapply(arbres,mean_var, var="Total_Fruits_per_m2")
+  sum_mast$meanTotal_Fruits_per_m2 = test
+  sum_mast
+
+}
+
 
 # Select the data per site
 
