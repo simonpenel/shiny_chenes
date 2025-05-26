30829bcb303e304117ed6f724da37a4b2c64b8bc
Simon Penel <simon.penel@univ-lyon1.fr>
2025-05-26 16:59:56 +0200

    Modif test3.R

--- mon_app/test3.R
+++ mon_app/test3.R
@@ -13,14 +13,26 @@ get_summary_2 <- function(m){
   test <- sapply(sites,extract_site,data=m,sum_site=sum_site)
   #print(test)
   #sum_site$Taux_moyen_arbres = test
-    #print(sum_site)
+  #print(sum_site)
 }
-mean_over_trees <- function(year,data){
+mean_over_trees <- function(year,data,sum){
 taux = data[data$Year == year ,]
-print(taux)
+site = unique(data$Site)
+print("Site")
+lol <-sum[sum$Site == site,]
+print("LOL")
+print("LOL")
+print(lol)
+print(site)
+#print(taux)
 taux = data[data$Year == year ,]$tauxfructif
 print(taux)
 mean_taux <- mean(taux)
+lol$mean_taux_over_trees = mean_taux
+#sum[sum$Site == site,]$mean_taux_over_trees = mean_taux
+#sum[sum$Year == year,]$mean_taux_over_trees = mean_taux
+print("LOLOLO")
+print(lol)
 mean_taux
 
 }
@@ -30,11 +42,12 @@ extract_site <- function(site,data,sum_site) {
 test <- data[data$Site == site ,]
 years <- unique(test$Year)
 #print(years)
-mean_year <- sapply(years,mean_over_trees,data=test)
-print(mean_year)
-sum_site[sum_site$Site == site,]$Taux_moyen_arbres = mean_year
+mean_year <- sapply(years,mean_over_trees,data=test,sum=sum_site)
+#print(mean_year)
+#sum_site[sum_site$Site == site,]$Taux_moyen_arbres = mean_year
 #sum_site[sum_site$Site == x,]$Taux_moyen_arbres = mean_year
-sum_site
+#sum_site
+mean_year
 }
 toto <- get_summary_2(masting)
-print(toto)
+#print(toto)
