fe256488274c946d093a7544319be93842d24013
Simon Penel <simon.penel@univ-lyon1.fr>
2025-05-28 16:43:09 +0200

    update test3.R

--- mon_app/test3.R
+++ mon_app/test3.R
@@ -4,64 +4,38 @@ options(encoding="latin1")
 masting <- read.csv("merged.csv",sep=";")
 
 
-
-get_summary_2 <- function(m){
-  sum_site  <- data.frame(Site=m$Site,Year=m$Year,Taux_moyen_arbres=0)
+# Fonction qui renvoie une df avec la valeur moyenne sur les arbres par site/an
+# ------------------------------------------------------------------------------
+get_summary_site <- function(m){
+  sum_site  <- data.frame(Site=m$Site,Year=m$Year,Moyenne_sur_les_arbres=0)
   sum_site <- unique(sum_site)
-  #print(sum_site)
   sites <- unique(sum_site$Site)
   test <- lapply(sites,extract_site,data=m,sum_site=sum_site)
-  #print(test)
-  #sum_site$Taux_moyen_arbres = test
-  #print(sum_site)
   test
 }
-mean_over_trees <- function(year,data,sum){
-taux = data[data$Year == year ,]
-site = unique(data$Site)
-#print(paste(c("Sites : ",data$Site)))
-#print(paste(c("Site : ",site)))
-lol <-sum[sum$Site == site,]
-#print(lol)
-#print(paste(c("Site = ",site)))
-#print(paste(c("Year = ",year)))
-#print(taux)
-taux = data[data$Year == year ,]$Total_Fruits_per_m2
-#print(taux)
-mean_taux <- mean(taux)
-print("MEAN TAUX")
-#print(mean_taux)
-#lol$mean_taux_over_trees = mean_taux
-#print(lol[lol$Year == year ,])
-#lol[lol$Year == year ,]$mean_taux_over_trees = mean_taux
-#sum[sum$Site == site,]$mean_taux_over_trees = mean_taux
-#sum[sum$Year == year,]$mean_taux_over_trees = mean_taux
-print("LOLOLO")
-#print(lol)
-mean_taux
 
+# Fonction qui calcule la valeur moyenne pour une annee donnee
+# ------------------------------------------------------------
+mean_over_trees <- function(year,data,var){
+  taux = data[data$Year == year ,][[var]]
+  mean_taux <- mean(taux)
+  mean_taux
 }
+
+# Fonction qui recupere les donnes pour un site, et renvoie une df
+# avec les valeurs moyenne d'une variable sur les arbres par annee
+# -----------------------------------------------------------------
 extract_site <- function(site,data,sum_site) {
-print("")
-print("")
-print("")
-print(paste(c("LOOP : SITE ",site)))
-#print(x)
-test <- data[data$Site == site ,]
-years <- unique(test$Year)
-#print(years)
-mean_year <- sapply(years,mean_over_trees,data=test,sum=sum_site)
-#print(years)
-#print(mean_year)
-sum_site[sum_site$Site == site,]$Taux_moyen_arbres = mean_year
-#print(sum_site[sum_site$Site == site,])
-sum_site_val  = sum_site[sum_site$Site == site,]
-#print(sum_site)
-#sum_site[sum_site$Site == x,]$Taux_moyen_arbres = mean_year
-#sum_site
-sum_site_val
+  test <- data[data$Site == site ,]
+  years <- unique(test$Year)
+  mean_year <- sapply(years,mean_over_trees,data=test,var="Total_Fruits_per_m2")
+  sum_site[sum_site$Site == site,]$Moyenne_sur_les_arbres = mean_year
+  sum_site_val  = sum_site[sum_site$Site == site,]
+  sum_site_val
 }
 
+# Fonction d'affichage
+# --------------------
 plot_site_years_var <- function(df,var, main, ylab, type = "b", pch = 19,
 xlab = "Year", leg = TRUE, posleg = "topleft", ...){
   nt   <- length(df)
@@ -69,11 +43,12 @@ xlab = "Year", leg = TRUE, posleg = "topleft", ...){
   type <- "b"
   pch <-19
 
-  #Recherche des maxs
+  # Recherche des maxs
   site_data <-df[[1]]
   ymax_all <- max(site_data[[var]])
   xmax_all <- max(site_data$Year)
   xmin_all  <- min(site_data$Year)
+
   for(j in 2:nt) {
     site_data <-df[[j]]
     ymax <- max(site_data[[var]])
@@ -84,16 +59,16 @@ xlab = "Year", leg = TRUE, posleg = "topleft", ...){
     xmin_all  <- min(c(xmin,xmin_all))
   }
 
-
   site_data <-df[[1]]
   site_data <-site_data[order(site_data$Year),,drop=FALSE]
+
   ylim <- c(0,ymax_all)
   xlim <- c(xmin_all - 3, xmax_all + 1)
+
   plot(site_data$Year, site_data[[var]], type = type, pch = pch, col = col[1],
    xlim=xlim, ylim = ylim, xlab = xlab, ylab = ylab, main = main,
    xaxp=c(xmin_all,xmax_all,xmax_all - xmin_all))
-  print("SITE = ")
-  print(unique(site_data$Site))
+
   liste_sites <- c(unique(site_data$Site))
 
   for(j in 2:nt) {
@@ -102,35 +77,9 @@ xlab = "Year", leg = TRUE, posleg = "topleft", ...){
     points(site_data$Year, site_data[[var]], type = type, pch = pch, col = col[j])
     liste_sites <- append(liste_sites, unique(site_data$Site))
   }
-  print(unique(df[[1]]$Site))
   legend("topleft", legend=liste_sites,box.lty=0,
        col=col, lty=1, cex=0.8)
 }
 
-
-toto <- get_summary_2(masting)
-plot_site_years_var(toto,"Taux_moyen_arbres", "Nb de fruit par m2 (moyenne)", "Nb de fruit par m2")
-
-#print(toto[1])
-#print(toto[2])
-
-#plot_site_years()
-
-#site_data <-toto[[1]]
-#site_data <-site_data[order(site_data$Year),,drop=FALSE]
-
-#print(site_data)
-#nt   <- length(toto)
-#col <- hcl.colors(nt, "Dark 2")
-#type <- "b"
-#pch <-19
-#ylim <- c(0, max(site_data$Taux_moyen_arbres))
-#xlim <- c(min(site_data$Year)-1, max(site_data$Year)+1)
-#plot(site_data$Year, site_data$Taux_moyen_arbres, type = type, pch = pch, col = col[1], xlim=xlim, ylim = ylim, xlab = "lol", ylab = "lol", main = "lol")
-#plot(site_data$Year, site_data$Taux_moyen_arbres, type = type, pch = pch, col = col[1])
-
-#for(j in 2:nt) {
-#site_data <-toto[[j]]
-#site_data <-site_data[order(site_data$Year),,drop=FALSE]
-#points(site_data$Year, site_data$Taux_moyen_arbres, type = type, pch = pch, col = col[j])
-#}
+toto <- get_summary_site(masting)
+plot_site_years_var(toto,"Moyenne_sur_les_arbres", "Nb de fruit par m2 (moyenne)", "Nb de fruit par m2")
